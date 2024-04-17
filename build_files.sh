60675029bb00a25e39f2bd5e65ae3ed55d9d88d5
joebaga <jlirenge@gmail.com>
2024-04-17 03:48:03 +0300

    Remove apt-get commands

--- build_files.sh
+++ build_files.sh
@@ -2,15 +2,11 @@ set -e
 
 echo "Building the project..."
 
-python3.9 -m pip install -r requirements.txt || exit 1
+python3.9 -m pip install -r requirements.txt || { echo "Failed to install requirements"; exit 1; }
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput || exit 1
-python3.9 manage.py migrate --noinput || exit 1
+python3.9 manage.py makemigrations --noinput || { echo "Failed to make migrations"; exit 1; }
+python3.9 manage.py migrate --noinput || { echo "Failed to migrate"; exit 1; }
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear || exit 1
-
-
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear || exit 1
+python3.9 manage.py collectstatic --noinput --clear || { echo "Failed to collect static"; exit 1; }
