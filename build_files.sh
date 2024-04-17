ea3d671b59c6cb5384439c63d61374dee353d3f5
joebaga <jlirenge@gmail.com>
2024-04-17 03:40:33 +0300

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,19 @@
-# Build the project
+set -e
+
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+
+
+git init || exit 1
+
+
+apt-get update
+apt-get install -y python3-pip || exit 1
+
+python3.9 -m pip install -r requirements.txt || exit 1
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput || exit 1
+python3.9 manage.py migrate --noinput || exit 1
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear || exit 1
--- comingsoon/urls.py
+++ comingsoon/urls.py
@@ -1,13 +1,13 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
+from django.conf import settings # type: ignore
+from django.conf.urls.static import static # type: ignore
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name=""),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
